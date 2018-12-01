e48c619cad945db2715f248c7cee7a98ed03580c
Ben Ousborne <bousborne@gmail.com>
2018-11-30 18:22:16 -0700

    Moving snow section to its' own component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
+import { SnowComponent } from './snow/snow.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SnowComponent
   ],
   imports: [
     BrowserModule,
